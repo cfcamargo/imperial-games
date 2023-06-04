ec5f7138c120ebe100caccd985173dbd30dd7936
Cristian Camargo <contato@camargodev.com.br>
2023-06-04 11:38:00 -0400

    fix : adiciona novidades

--- src/Components/Banner/index.tsx
+++ src/Components/Banner/index.tsx
@@ -25,19 +25,31 @@ export function Banner() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <div className='w-full h-[calc(100vh_-_80px)] flex justify-center items-center bg-slideportada bg-cover bg-center'>
 
+          </div>
+        </SwiperSlide> */}
+        <SwiperSlide>
+          <div className='w-full h-[calc(100vh_-_80px)] flex justify-center items-center bg-machinenew bg-cover bg-center'>
+            <div className='w-full h-full bg-black bg-opacity-80 flex items-center justify-center flex-col gap-4'>
+              <h2 className='text-dark-yellow xs:text-xl md:text-4xl font-bold uppercase'>
+                <FormattedMessage id="banner-new-title"/>
+              </h2>
+              <p className='text-light-yellow xs:text-xs md:text-lg xs:max-w-[300px] md:max-w-2xl'>
+                <FormattedMessage id="banner-new-text-1"/>
+              </p>
+            </div>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
           <div className='w-full h-[calc(100vh_-_80px)] flex justify-center items-center bg-slide1 bg-cover bg-center'>
             <div className='w-full h-full bg-black bg-opacity-80 flex items-center justify-center flex-col gap-4'>
-              <p className='text-light-yellow xs:text-sm md:text-lg'>
+              <p className='text-light-yellow xs:text-xs md:text-lg'>
                 <FormattedMessage id="banner1-text"/>
               </p>
-              <h2 className='text-dark-yellow xs:text-3xl md:text-5xl font-bold uppercase'>
+              <h2 className='text-dark-yellow xs:text-xl md:text-5xl font-bold uppercase'>
                 <FormattedMessage id="banner1-title"/>
               </h2>
             </div>
@@ -48,9 +60,9 @@ export function Banner() {
           <div className='w-full h-[calc(100vh_-_80px)] flex justify-center items-center bg-slide2 bg-cover bg-center'>
             <div className='w-full h-full bg-black bg-opacity-80 flex items-center justify-center flex-col'>
               <div className="containeer flex xs:items-center md:items-start justify-center flex-col gap-4">
-                <h2 className='text-dark-yellow xs:text-2xl md:text-5xl font-bold uppercase'><FormattedMessage id="banner2-title"/></h2>
-                <p className='text-light-yellow xs:text-sm md:text-lg'><FormattedMessage id="banner2-text"/></p>
-                <p className='text-light-yellow xs:text-sm md:text-lg'><FormattedMessage id="banner2-text2"/></p>
+                <h2 className='text-dark-yellow xs:text-xl md:text-5xl font-bold uppercase'><FormattedMessage id="banner2-title"/></h2>
+                <p className='text-light-yellow xs:text-xs md:text-lg'><FormattedMessage id="banner2-text"/></p>
+                <p className='text-light-yellow xs:text-xs md:text-lg'><FormattedMessage id="banner2-text2"/></p>
               </div>
             </div>
           </div>
@@ -60,10 +72,10 @@ export function Banner() {
           <div className='w-full h-[calc(100vh_-_80px)] flex justify-center items-center bg-slide3 bg-cover bg-center'>
             <div className='w-full h-full bg-black bg-opacity-80 flex items-center justify-center flex-col gap-4'>
               <div className="containeer flex xs:items-center md:items-end justify-center flex-col gap-4">
-                <h2 className='text-dark-yellow xs:text-2xl md:text-5xl font-bold uppercase '>
+                <h2 className='text-dark-yellow xs:text-xl md:text-5xl font-bold uppercase '>
                   <FormattedMessage id="banner3-title"/>
                 </h2>
-                <p className='text-light-yellow xs:text-center md:text-right xs:max-w-[300px] md:max-w-[600px] xs:text-sm md:text-lg'>
+                <p className='text-light-yellow xs:text-center md:text-right xs:max-w-[300px] md:max-w-[600px] xs:text-xs md:text-lg'>
                   <FormattedMessage id="banner3-text"/>
                 </p>
               </div>
@@ -75,10 +87,10 @@ export function Banner() {
           <div className='w-full h-[calc(100vh_-_80px)] flex justify-center items-center bg-slide4 bg-cover bg-center'>
             <div className='w-full h-full bg-black bg-opacity-80 flex items-center justify-center flex-col gap-4'>
               <div className="containeer flex items-center justify-center flex-col gap-4">
-                <h2 className='text-dark-yellow xs:text-2xl md:text-5xl font-bold uppercase'>
+                <h2 className='text-dark-yellow xs:text-xl md:text-5xl font-bold uppercase'>
                   <FormattedMessage id="banner4-title"/>
                 </h2>
-                <p className='text-light-yellow xs:text-sm md:text-lg'>
+                <p className='text-light-yellow xs:text-xs md:text-lg xs:max-w-[300px] md:max-w-2xl'>
                   <FormattedMessage id="banner4-text"/>
                 </p>
               </div>
--- src/Components/New/index.tsx
+++ src/Components/New/index.tsx
@@ -1,23 +1,22 @@
 import { FormattedMessage } from 'react-intl';
-import { VideoPlayer } from '../VideoPlayer';
+import news1Img from '../../assets/news1.png'
+import news2Img from '../../assets/news2.png'
+import news3Img from '../../assets/news3.png'
 
 export function New(){
     return (
         <div className="containeer py-20">
             <div className="flex flex-col items-center">
                 <h3 className="text-center text-3xl font-bold text-dark-yellow">
-                    <FormattedMessage id="new-title"/>
+                    <FormattedMessage id="news-container-title"/>
                 </h3>
-                <p className="mt-6 text-light-yellow font-bold">
-                    <FormattedMessage id="new-text-1"/>
-                </p>
-                <p className="text-light-yellow">
-                    <FormattedMessage id="new-text-2"/>
-                </p>
-            </div>
 
-            <div className='my-20'>
-                <VideoPlayer videoURL='https://youtu.be/AuV9zUziJes'/>
+                <div className='grid xs:grid-cols-1 md:grid-cols-3 gap-4 py-10'>
+                    <img src={news1Img} alt="" className='w-full object-cover aspect-[2/3]'/>
+                    <img src={news2Img} alt="" className='w-full object-cover aspect-[2/3]'/>
+                    <img src={news3Img} alt="" className='w-full object-cover aspect-[2/3]'/>
+                </div>
+
             </div>
         </div>
     )
